b19f62816a6f835011a0221353df1456e1a1cf7f
Kaylie V <k.verner37@gmail.com>
2020-05-14 11:15:42 -0400

    updating file, successfully sent post request with postman

--- Develop/public/server.js
+++ Develop/public/server.js
@@ -1,5 +1,7 @@
 var express = require("express");
 var path = require("path");
+var fs = require("fs");
+const data = require("../db/db.json")
 
 var app = express();
 var PORT = process.env.PORT || 3000;
@@ -8,7 +10,12 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 //Notes
-var notes = []; 
+var notes = [];
+
+fs.appendFile('db.json', notes, function (err) {
+    if (err) throw err;
+    console.log('Saved!');
+  });
 
 //Routes to send user to pages 
 app.get("/notes", function (req, res) {
@@ -20,23 +27,38 @@ app.get("*", function (req, res) {
 });
 
 app.get("/api/notes", function (req, res) {
-    return res.json("db.json");
+    // fs.readFile(data, (err, data) => {
+    //     if (err) throw err;
+    //     res.json(data);
+    // });
+    res.json(data);
 });
 
 //Post
 app.post("/api/notes", function (req, res) {
+    // newNote.id = Math.random() * 100;
     var newNote = req.body;
-
     newNote.routeName = newNote.name;
-
+    notes.push(newNote);
     res.json(newNote);
+
+    fs.appendFile("db.json", JSON.parse(notes), function (err) {
+        if (err) throw err;
+        console.log('Updated!');
+      });
+
+    // var newNote = req.body;
+
+    // newNote.routeName = newNote.name;
+
+    // res.json(newNote);
 });
 
 //Delete 
 app.delete("/api/notes/:id", function (req, res) {
-    var chosen = req.params.id; 
+    var chosen = req.params.id;
 
-    console.log(id); 
+    console.log(id);
 
     for (var i = 0; i < notes.length; i++) {
 
